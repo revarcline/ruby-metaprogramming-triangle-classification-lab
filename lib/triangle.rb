44aa7ee3b4227f4126f4c434d6cd52faf3821a89
Alex Cline <rev.a.r.cline@gmail.com>
2020-12-21 21:09:43 -0600

    commit tree 41dd15e9b9347964b00fb3d852345af60c767ef4

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  attr_reader :one, :two, :three
+
+  def initialize(one, two, three)
+    @one = one
+    @two = two
+    @three = three
+  end
+
+  def valid?
+    inequal = one + two > three && one + three > two && two + three > one
+    lengths = one.positive? && two.positive? && three.positive?
+    inequal && lengths
+  end
+
+  def kind
+    raise TriangleError unless valid?
+
+    if one == two && two == three
+      :equilateral
+    elsif one == three || two == three || one == two
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
